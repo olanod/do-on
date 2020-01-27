a984c4291fb59dfea4beb0927f0170b9d9f7120a
Daniel Olano <danielolanoc@gmail.com>
2020-01-27 16:38:42 +0100

    Initial implementation

---
+++ README.md
@@ -0,0 +1,4 @@
+# do-on element
+
+Simple custom build-in element to be used with `<template>`. It listens to iterable messages from a `BroadcastChannel` configured with the attribute `channel="name-of-channel"` and appends to or replaces the contents to the template's parent.
+
---
+++ on.js
@@ -0,0 +1,46 @@
+const moduleFromString = async str => import(
+	URL.createObjectURL(new Blob([str], {type: 'application/javascript'}))
+)
+
+const removeSiblings = ele => {
+	while (ele.previousSibling) ele.previousSibling.remove()
+	while (ele.nextSibling) ele.nextSibling.remove()
+}
+
+export class On extends HTMLTemplateElement {
+	#chan
+	#script
+	#onMsg = ({data}) => {
+		data = data.map(d => {
+			this.updateContent(this.content, d)
+			return this.content.cloneNode(true)
+		})
+		requestAnimationFrame(() => {
+			if (this.mode === 'replace') removeSiblings(this)
+			this.parentElement.append(...data)
+		})
+	}
+
+	mode = 'append'
+	channel = ''
+	updateContent = () => {}
+
+	constructor() {
+		super()
+		this.#script = this.content.querySelector('script')
+		this.content.removeChild(this.#script)
+		this.channel = this.getAttribute('channel')
+		if (this.channel)
+			this.#chan = new BroadcastChannel(this.channel)
+	}
+
+	async connectedCallback() {
+		if (this.#script) {
+			let {update} = await moduleFromString(this.#script.textContent)
+			this.updateContent = update
+		}
+		if (this.#chan)
+			this.#chan.addEventListener('message', this.#onMsg)
+	}
+}
+customElements.define('do-on', On, {extends: 'template'})
